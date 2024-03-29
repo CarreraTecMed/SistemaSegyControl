79fb7faa0cd090503b1d94811d9d5a95d528e2ac
Enrique Luis <barra.paredes.enrique.luis@gmail.com>
2024-03-28 23:17:59 -0400

    Listo servidor

--- resources/app/layout/AuthLayout.jsx
+++ resources/app/layout/AuthLayout.jsx
@@ -1,14 +1,39 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import loginImg from '../public/images/fondo.jpeg';
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import { useEffect } from "react";
+import useProyect from "../hooks/useProyect";
 
 export default function AuthLayout() {
 
+    const { changeView } = useProyect()
+    const navigate = useNavigate()
+
     useEffect(() => {
-        
-    },[])
+        const userLogged = JSON.parse(localStorage.getItem('usuario'))
+        if (Boolean(userLogged)) {
+            if (userLogged.tipo == 'estudiante') {
+                navigate('/estudiante')
+                changeView('materiales')
+            }
+
+            if (userLogged.tipo == 'administrativo') {
+                navigate('/administrativo/correspondencia-recibida')
+                changeView('correspondencia recibida')
+            }
+
+            if (userLogged.tipo == 'administrador') {
+                navigate('/administrador/usuarios')
+                changeView('usuarios')
+            }
+
+            if (userLogged.tipo == 'colaborador') {
+                navigate('/colaborador/correspondencia')
+                changeView('correspondencia')
+            }
+        }
+    }, [])
 
     return (
         <>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel + React</title>
+        <link rel="shortcut icon" type="image/png" href="{{ asset('/images/logoTecMed.png') }}">
+        {{-- <link rel="shortcut icon" sizes="192x192" href="{{ asset('/images/logoTecMed.png') }}"> --}}
+        <title>TecMed Documentacion</title>
 
         @viteReactRefresh
 
