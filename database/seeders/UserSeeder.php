c40df5c5f4c4224d5db75479b3514e74c25d7af7
Enrique Luis <barra.paredes.enrique.luis@gmail.com>
2024-05-07 20:58:11 -0400

    Revision final

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,16 +15,16 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         DB::table('users')->insert([
-            'ci' => '8441659',
-            'nombres' => 'Enrique L.',
-            'apellidoPaterno' => 'Barra',
-            'apellidoMaterno' => 'Paredes',
+            'ci' => '8441650',
+            'nombres' => 'Lic. Delia',
+            'apellidoPaterno' => 'Nina',
+            'apellidoMaterno' => 'Huanca de Echegaray',
             'tipo' => 'administrador',
             'estado' => 'activo',
             'imagen' => 'adminFoto.jpg',
             'fechaNacimiento' => '2001-05-03',
-            'email' => 'barra.paredes.enrique.luis@gmail.com',
-            'password' => bcrypt('Rildito123.'),
+            'email' => 'deliusnh16@gmail.com',
+            'password' => bcrypt('Delia123.'),
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
